3c35c829bad861666c182ddddd6430946bf5fa8e
= <=>
2022-04-04 14:16:49 +0700

    remove css

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 8888
-
-app.get('/', (req, res) => res.send("Hello Tao Le!"))
-
-
-app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
